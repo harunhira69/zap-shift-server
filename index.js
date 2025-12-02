5e32677b5772928dd7c398d9803a10eb58db13d9
harunhira69 <harunabhi4@gmail.com>
2025-12-02 17:12:21 +0600

    added payment api

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
 
 const app = express()
 require('dotenv').config();
-const port = process.env.PORT||3000
+const port = process.env.PORT || 3000
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irtmkrl.mongodb.net/?appName=Cluster0`;
 
 app.use(express.json());
@@ -35,40 +38,85 @@ async function run() {
     const percelCollection = db.collection('parcels')
 
     // parcel api
-    app.get('/parcel',async(req,res)=>{
-      const query ={};
-      const {email} = req.query
+    app.get('/parcel', async (req, res) => {
+      const query = {};
+      const { email } = req.query
 
-        if(email){
+      if (email) {
         query.senderEmail = email;
-        
+
       }
-      const option = {sort:{createdAt:-1}}
-      const cursor = percelCollection.find(query,option);
+      const option = { sort: { createdAt: -1 } }
+      const cursor = percelCollection.find(query, option);
       const result = await cursor.toArray();
       res.send(result)
-    
+
 
     })
 
-    app.post('/parcel',async(req,res)=>{
+    app.post('/parcel', async (req, res) => {
       const parcel = req.body;
       parcel.createdAt = new Date();
       const result = await percelCollection.insertOne(parcel);
       res.send(result)
     });
 
-  app.delete('/parcel/:id', async (req, res) => {
-  const id = req.params.id;
-  console.log("Deleting parcel:", id);
+    app.delete('/parcel/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log("Deleting parcel:", id);
 
-  const query = { _id: new ObjectId(id) };
-  const result = await percelCollection.deleteOne(query);
-  res.send(result);
+      const query = { _id: new ObjectId(id) };
+      const result = await percelCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get('/parcel/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await percelCollection.findOne(query);
+      res.send(result);
+
+
+    })
+ app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const paymentInfo = req.body;
+    const amount = parseInt(paymentInfo.cost) * 100;
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'USD',
+            unit_amount: amount,
+            product_data: {
+              name: paymentInfo.parcelName,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      customer_email: paymentInfo.senderEmail,
+      metadata: {
+        parcelId: paymentInfo.parcelId,
+      },
+      success_url: `${process.env.SITE_DOMAIN}dashboard/payment-success`,
+      cancel_url: `${process.env.SITE_DOMAIN}dashboard/payment-cancelled`,
+    });
+
+    res.send({ url: session.url });
+
+  } catch (error) {
+    console.error("Stripe Error:", error);
+    res.status(400).send({ error: error.message });
+  }
 });
 
 
-  
+
+
+
 
 
 
